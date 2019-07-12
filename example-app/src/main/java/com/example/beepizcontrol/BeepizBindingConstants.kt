ea0e5607bec8ea6ebbb9bc6242703ff7be5115c0
Louis CAD <louis.cognault@gmail.com>
2019-07-12 10:18:51 +0200

    Add KDoc on constants

--- example-app/src/main/java/com/example/beepizcontrol/BeepizBindingConstants.kt
+++ example-app/src/main/java/com/example/beepizcontrol/BeepizBindingConstants.kt
@@ -1,14 +1,31 @@
 package com.example.beepizcontrol
 
+import android.content.Intent
 import android.os.Message
 import android.os.Messenger
 
 object BeepizBindingConstants {
 
+    /**
+     * The package name of the Beepiz application.
+     *
+     * You'll need it to check if it is installed and to launch it if it requires configuration.
+     */
     const val packageName = "net.suivideflotte.dati"
 
+    /**
+     * The **permission required** by Beepiz to be controlled by a third-party app.
+     * It is a runtime permission on API level 23+.
+     *
+     * The `AndroidManifest.xml` of the app must also include the permission as such:
+     * `<uses-permission android:name="net.suivideflotte.dati.permission.CONTROL_MONITORING" />`
+     */
     const val permission = "$packageName.permission.CONTROL_MONITORING"
 
+    /**
+     * The action required in the [Intent] to bind to the service that allows listening the
+     * monitoring and configuration state of Beepiz.
+     */
     const val bindAction = "bind_monitoring_state"
 
     /**
@@ -50,14 +67,14 @@ object BeepizBindingConstants {
     const val ARG1_CONFIG_OK = 0
 
     /**
-     * Action to broadcast to Beepiz to start monitoring. Be sure to specify the Intent
-     * targetPackage or the system will ignore it.
+     * Action to broadcast to Beepiz to start monitoring. **Be sure to specify the Intent
+     * targetPackage or the system will ignore it.**
      */
     const val startAction = "start_monitoring"
 
     /**
-     * Action to broadcast to Beepiz to stop monitoring. Be sure to specify the Intent
-     * targetPackage or the system will ignore it.
+     * Action to broadcast to Beepiz to stop monitoring. **Be sure to specify the Intent
+     * targetPackage or the system will ignore it.**
      */
     const val stopAction = "stop_monitoring"
 }
